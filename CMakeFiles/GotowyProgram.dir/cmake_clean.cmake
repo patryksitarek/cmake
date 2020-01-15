55038b80433da82eb1725dd78fdb8fdda047c3a3
patryksitarek <58287067+patryksitarek@users.noreply.github.com>
2020-01-15 23:17:16 +0100

    Add files via upload

---
+++ CMakeFiles/GotowyProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GotowyProgram.dir/src/hw_hello.c.o"
+  "CMakeFiles/GotowyProgram.dir/src/hw_main.c.o"
+  "CMakeFiles/GotowyProgram.dir/src/hw_power.c.o"
+  "GotowyProgram.pdb"
+  "GotowyProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GotowyProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
